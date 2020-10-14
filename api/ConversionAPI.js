6cf4e12e37db3609649a031b4eee54db38166615
Numerix <42088870+Numerix76@users.noreply.github.com>
2020-10-14 22:56:21 +0200

    Update ConversionAPI.js

--- api/ConversionAPI.js
+++ api/ConversionAPI.js
@@ -1,14 +1,18 @@
 const got = require('got');
 
 const makeURLYT = (query) => `http://92.222.234.121:8080/${encodeURIComponent(query)}`;
+const makeURLYTStatus = (query) => `http://92.222.234.121:8080/logs/${encodeURIComponent(query)}.txt`;
+
 const makeURLSC = (query) => `http://92.222.234.121/soundcloud/download.php?url=${encodeURIComponent(query)}`;
 
 class ConversionAPI {
     async conversion(query, yt) {
         if (yt) {
-            var res = await got(makeURLYT(query), {
-                responseType: 'json'
-            })
+            if ( query.length == 11 ) {
+                var res = await got(makeURLYT(query), {
+                    responseType: 'json'
+                })
+            }
         }
         else {
             var res = await got(makeURLSC(query), {
@@ -17,9 +21,23 @@ class ConversionAPI {
         }
 
         if (!res || !res.body) {
+            return ""
+        }
+        
+        console.log(res.body)
+        return res.body
+    }
+
+    async getstatusYT(query) {
+        const res = await got(makeURLYTStatus(query), {
+            responseType: 'json'
+        })
+
+        if (!res || !res.body) {
             throw new Error('Invalid response of Numerix API')
         }
 
+        console.log(res.body)
         return res.body
     }
 }
