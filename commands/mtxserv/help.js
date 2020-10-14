34f7dbbaf42590c989190ac7449eb229076b3a48
Seb <s.dieunidou@gmail.com>
2020-10-14 11:55:12 +0200

    fix help command

--- commands/mtxserv/help.js
+++ commands/mtxserv/help.js
@@ -1,6 +1,7 @@
 const { stripIndents, oneLine } = require('common-tags');
 const { disambiguation } = require('discord.js-commando/src/util');
 const mTxServCommand = require('../mTxServCommand.js');
+const Discord = require('discord.js');
 
 module.exports = class HelpCommand extends mTxServCommand {
 	constructor(client) {
@@ -29,35 +30,47 @@ module.exports = class HelpCommand extends mTxServCommand {
 		const commands = this.client.registry.findCommands(args.command, false, msg);
 		const showAll = args.command && args.command.toLowerCase() === 'all';
 
+		const lang = require(`../../languages/${this.getLangOfMember(msg.member)}.json`);
+
 		if(args.command && !showAll) {
 			if(commands.length === 1) {
-				let help = stripIndents`
+				const embed = new Discord.MessageEmbed()
+					.setTitle(':question: ' + commands[0].name)
+					.setColor('BLUE')
+					.setDescription(stripIndents`
+					${commands[0].description}
 					${oneLine`
-						__Command **${mTxServCommands[0].name}**:__ ${mTxServCommands[0].description}
-						${mTxServCommands[0].guildOnly ? ' (Usable only in servers)' : ''}
-						${mTxServCommands[0].nsfw ? ' (NSFW)' : ''}
-					`}
+						${commands[0].guildOnly ? ' (Usable only in servers)' : ''}
+						${commands[0].nsfw ? ' (NSFW)' : ''}
+					`}`)
+					.addField('Format', `${msg.anyUsage(`${commands[0].name}${commands[0].format ? `${commands[0].format}` : ''}`)}`)
+					.addField('Group', `${commands[0].group.name} (\`${commands[0].groupID}:${commands[0].memberName}\`)`)
+				;
 
-					**Format:** ${msg.anyUsage(`${mTxServCommands[0].name}${mTxServCommands[0].format ? ` ${mTxServCommands[0].format}` : ''}`)}
-				`;
-				if(commands[0].aliases.length > 0) help += `\n**Aliases:** ${mTxServCommands[0].aliases.join(', ')}`;
-				help += `\n${oneLine`
-					**Group:** ${mTxServCommands[0].group.name}
-					(\`${mTxServCommands[0].groupID}:${mTxServCommands[0].memberName}\`)
-				`}`;
-				if(commands[0].details) help += `\n**Details:** ${mTxServCommands[0].details}`;
-				if(commands[0].examples) help += `\n**Examples:**\n${mTxServCommands[0].examples.join('\n')}`;
 
-				const messages = [];
-				try {
-					messages.push(await msg.direct(help));
-					if(msg.channel.type !== 'dm') messages.push(await msg.reply('Sent you a DM with information.'));
-				} catch(err) {
-					messages.push(await msg.reply('Unable to send you the help DM. You probably have DMs disabled.'));
+				if(commands[0].aliases.length)  {
+					embed.addField('Aliases', commands[0].aliases.join(', '))
 				}
-				return messages;
+
+				if(commands[0].details)  {
+					embed.addField('Details', commands[0].details)
+				}
+
+				if(commands[0].examples)  {
+					embed.addField('Examples', commands[0].examples.join('\n'))
+				}
+
+				try {
+					if(msg.channel.type !== 'dm') await msg.reply(['help']['sent_dm']);
+
+					return msg.author.send({
+						embed
+					});
+				} catch(err) {
+					return msg.reply(lang['help']['dm_closed']);
+				}
 			} else if(commands.length > 15) {
-				return msg.reply('Multiple commands found. Please be more specific.');
+				return msg.reply(lang['help']['multiple_commands']);
 			} else if(commands.length > 1) {
 				return msg.reply(disambiguation(commands, 'commands'));
 			} else {
@@ -68,13 +81,16 @@ module.exports = class HelpCommand extends mTxServCommand {
 				);
 			}
 		} else {
-			const messages = [];
 			try {
-				messages.push(await msg.direct(stripIndents`
+				const embed = new Discord.MessageEmbed()
+					.setTitle(':question: Help')
+					.setColor('BLUE')
+					.setDescription(stripIndents`
 					${oneLine`
 						To run a command, use ${mTxServCommand.usage('command', msg.guild ? msg.guild.commandPrefix : null, this.client.user)}.
-						For example, ${mTxServCommand.usage('help', msg.guild ? msg.guild.commandPrefix : null, this.client.user)}.
+						, for example, ${mTxServCommand.usage('help', msg.guild ? msg.guild.commandPrefix : null, this.client.user)}.
 					`}
+					
 					To run a command in this DM, simply use ${mTxServCommand.usage('command', null, null)} with no prefix.
 
 					Use ${this.usage('<command>', null, null)} to view detailed information about a specific command.
@@ -83,18 +99,23 @@ module.exports = class HelpCommand extends mTxServCommand {
 
 					${groups.filter(grp => grp.commands.some(cmd => !cmd.hidden && cmd.isUsable(msg)))
 						.map(grp => stripIndents`
-							__${grp.name}__
+							**__${grp.name}__**
 							${grp.commands.filter(cmd => !cmd.hidden && cmd.isUsable(msg))
-								.map(cmd => `**${cmd.name}:** ${cmd.description}${cmd.nsfw ? ' (NSFW)' : ''}`).join('\n')
-							}
+							.map(cmd => `**${cmd.name}:** ${cmd.description}${cmd.nsfw ? ' (NSFW)' : ''}`).join('\n')
+						}
 						`).join('\n\n')
 					}
-				`, { split: true }));
-				if(msg.channel.type !== 'dm') messages.push(await msg.reply('Sent you a DM with information.'));
+				`)
+				;
+
+				if(msg.channel.type !== 'dm') await msg.reply(['help']['sent_dm']);
+
+				return msg.author.send({
+					embed
+				});
 			} catch(err) {
-				messages.push(await msg.reply('Unable to send you the help DM. You probably have DMs disabled.'));
+				return msg.reply(lang['help']['dm_closed']);
 			}
-			return messages;
 		}
 	}
 };
