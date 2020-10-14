680581497518c08e3a4209a29f7d419982045534
Seb <s.dieunidou@gmail.com>
2020-10-14 15:03:35 +0200

    add rule command

---
+++ commands/mtxserv/rules.js
@@ -0,0 +1,33 @@
+const mTxServCommand = require('../mTxServCommand.js');
+const Discord = require('discord.js');
+
+module.exports = class SocialCommand extends mTxServCommand {
+    constructor(client) {
+        super(client, {
+            name: 'rules',
+            aliases: ['regle', 'regles', 'reglement', 'rule'],
+            group: 'mtxserv',
+            memberName: 'rules',
+            description: 'Get rules of this Discord',
+            clientPermissions: ['SEND_MESSAGES'],
+            throttling: {
+                usages: 2,
+                duration: 5,
+            },
+        });
+    }
+
+    async run(msg) {
+        const lang = require(`../../languages/${this.resolveLangOfMessage(msg)}.json`);
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(lang['rules']['title'])
+            .setColor('BLUE')
+            .setDescription(lang['rules']['description'])
+        ;
+
+        return msg.say({
+            embed
+        });
+    }
+};
