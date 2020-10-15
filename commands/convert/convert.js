19683b1d83ab6bfd1b93e8fd26e6a9114d60bcf6
Seb <s.dieunidou@gmail.com>
2020-10-15 11:10:45 +0200

    fixes

--- commands/convert/convert.js
+++ commands/convert/convert.js
@@ -1,6 +1,7 @@
 const mTxServCommand = require('../mTxServCommand.js')
 const ConverterApi = require('../../api/ConverterApi.js')
 const Discord = require('discord.js')
+const UrlParser = require('js-video-url-parser')
 
 module.exports = class ConvertCommand extends mTxServCommand {
     constructor(client) {
@@ -11,12 +12,17 @@ module.exports = class ConvertCommand extends mTxServCommand {
             memberName: 'convert-mp3',
             description: 'Convert Youtube Videos or SoundCloud Music to MP3',
             clientPermissions: ['SEND_MESSAGES'],
+            ownerOnly: true,
+            hidden: true,
             args: [
                 {
                     key: 'query',
                     prompt: 'Which music (Youtube or SoundCloud URL) do you want to convert?',
                     type: 'string',
-                    validate: text => text.length >= 3,
+                    validate: url => {
+                        const details = UrlParser.parse(url)
+                        return details.provider === 'youtube' || details.provider === 'soundcloud'
+                    }
                 },
             ],
             throttling: {
@@ -27,29 +33,33 @@ module.exports = class ConvertCommand extends mTxServCommand {
     }
 
     async run(msg, { query }) {
+        const details = UrlParser.parse(query)
+
+        const isYoutube = details.provider === 'youtube'
+
+        msg.react('🔃');
+
         const api = new ConverterApi()
-        var yt = false
-        
-        var video_id = query.split('v=')[1] || query; // On récupère l'ID YT ou on garde le lien soundcloud
-        var ampersandPosition = video_id.indexOf('&');
-        if(ampersandPosition != -1) {
-          video_id = video_id.substring(0, ampersandPosition);
-        }
-        
-        if ( video_id != query ) { // On regarde si on va traiter une vidéo YT
-          var query = video_id
-          yt = true 
-        }
+        let results;
 
-        msg.react('✅');
+        switch (details.provider) {
+            case 'youtube':
+                results = await api.convertYoutube(details.id)
+                break;
 
-        const results = await api.conversion(query, yt) || false
+            case 'soundcloud':
+                results = await api.convertSoundcloud(query)
+                break;
+
+            default:
+                throw new Error(`Provider ${details.provider} is not managed`)
+        }
 
         // On check si on a un résultat correct
         setTimeout(() => {
             if ( results == "" || (results.error && results.error != "") ) {
                 msg.react('❌');
-                msg.reactions.cache.get('✅').remove().catch(error => console.error('Failed to remove reactions: ', error));
+                msg.reactions.cache.get('✅').remove();
                 return
             }
         }, 1000)
@@ -62,38 +72,40 @@ module.exports = class ConvertCommand extends mTxServCommand {
             .setTimestamp()
         ;
 
-        if (!yt || results.already) {
+        if (!isYoutube || results.already) {
             embed.addField('La musique est disponible :', results.link);
-        }        
-        
+        }
+
         if ( results != "" || (results.error && results.error != "") ) {
             msg.channel.send({embed}) // Use a 2d array?
             .then(async function (message) {
-                if (yt && !results.already && results) {
+                if (isYoutube && !results.already && results) {
                     const intervalObj = setInterval(() => {
-                    // Re-fetch the message and get reaction counts
-                    message.channel.messages.fetch(message.id)
-                    .then(async function (message) {
-                        const embed2 = new Discord.MessageEmbed()
-                            .setTitle(`Conversion de : ${phrase}`)
-                            .setColor('BLUE')
-                            .setTimestamp()
+                        // Re-fetch the message and get reaction counts
+                        message.channel.messages
+                            .fetch(message.id)
+                            .then(async function (message) {
+                                const embed2 = new Discord.MessageEmbed()
+                                    .setTitle(`Conversion de : ${phrase}`)
+                                    .setColor('BLUE')
+                                    .setTimestamp()
+                                ;
+
+                                const result = await api.getstatusYT(query)
+
+                                if (result.progress.percentage == 100) {
+                                    embed2.addField('La musique est disponible :', results.link);
+
+                                    clearInterval(intervalObj);
+                                } else {
+                                    embed2.addField("Conversion en cours :", `${Math.round(result.progress.percentage)} %` )
+                                    embed2.addField("Temps estimée :", result.progress.eta)
+                                    embed2.setTimestamp();
+                                }
+
+                                message.edit("", embed2);
+                            })
                         ;
-                        
-                        const result = await api.getstatusYT(query)
-                        
-                        if (result.progress.percentage == 100) {
-                            embed2.addField('La musique est disponible :', results.link);
-
-                            clearInterval(intervalObj);
-                        } else {
-                            embed2.addField("Conversion en cours :", `${Math.round(result.progress.percentage)} %` )
-                            embed2.addField("Temps estimée :", result.progress.eta)
-                            embed2.setTimestamp();
-                        }
-
-                        message.edit("", embed2);
-                    });
                     }, 1000);
                 }
 
