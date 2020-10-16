1eb33019de4f84cb01b4433f3f55dfaa7f536145
Numerix <42088870+Numerix76@users.noreply.github.com>
2020-10-16 11:47:10 +0200

    Rust, Gmod, S&box news (#4)

---
+++ commands/news/rust.js
@@ -0,0 +1,46 @@
+const mTxServCommand = require('../mTxServCommand.js');
+const RssFeederApi = require('../../api/RssFeederApi')
+const Discord = require('discord.js');
+const striptags = require('striptags')
+
+module.exports = class NewsRustCommand extends mTxServCommand {
+    constructor(client) {
+        super(client, {
+            name: 'rust-news',
+            aliases: ['news-rust'],
+            group: 'news',
+            memberName: 'rust',
+            description: 'Show latest blog posts from rust.facepunch.com',
+            clientPermissions: ['SEND_MESSAGES'],
+            throttling: {
+                usages: 2,
+                duration: 5,
+            },
+        });
+    }
+
+    async run(msg) {
+        const api = new RssFeederApi();
+        const feed = await api.get('https://rust.facepunch.com/rss/blog');
+        const articles = Object.values(feed.items)
+
+        const embed = new Discord.MessageEmbed()
+            .setDescription(`${feed.description}\n<${feed.link}>`)
+            .setColor('BLUE')
+            .setTimestamp()
+            .setAuthor(feed.title, `https://mtxserv.com/build/manager-game/img/game/rust.png`)
+        ;
+
+        for (const k in articles.slice(0, 3)) {
+            let content = articles[k]['contentSnippet'] || articles[k]['content']
+            content = striptags(content).replace(/(?:https?|ftp):\/\/[\n\S]+/g, '').trim();
+            content = content.length > 200 ? content.substr(0, 200) : content
+
+            embed.addField(`__:writing_hand: ${articles[k].title}__`, `${content}\n:link: <${articles[k].link}>`)
+        }
+
+        return msg.say({
+            embed
+        });
+    }
+};
---
+++ commands/news/sandbox.js
@@ -0,0 +1,46 @@
+const mTxServCommand = require('../mTxServCommand.js');
+const RssFeederApi = require('../../api/RssFeederApi')
+const Discord = require('discord.js');
+const striptags = require('striptags')
+
+module.exports = class NewsSandboxCommand extends mTxServCommand {
+    constructor(client) {
+        super(client, {
+            name: 'sandbox',
+            aliases: ['s&box', 'gmod2'],
+            group: 'news',
+            memberName: 'sandbox',
+            description: 'Show latest blog posts from sandbox.facepunch.com',
+            clientPermissions: ['SEND_MESSAGES'],
+            throttling: {
+                usages: 2,
+                duration: 5,
+            },
+        });
+    }
+
+    async run(msg) {
+        const api = new RssFeederApi();
+        const feed = await api.get('https://sandbox.facepunch.com/rss/news');
+        const articles = Object.values(feed.items)
+
+        const embed = new Discord.MessageEmbed()
+            .setDescription(`${feed.description}\n<${feed.link}>`)
+            .setColor('BLUE')
+            .setTimestamp()
+            .setAuthor(feed.title)
+        ;
+
+        for (const k in articles.slice(0, 3)) {
+            let content = articles[k]['contentSnippet'] || articles[k]['content']
+            content = striptags(content).replace(/(?:https?|ftp):\/\/[\n\S]+/g, '').trim();
+            content = content.length > 200 ? content.substr(0, 200) : content
+
+            embed.addField(`__:writing_hand: ${articles[k].title}__`, `${content}\n:link: <${articles[k].link}>`)
+        }
+
+        return msg.say({
+            embed
+        });
+    }
+};
