2727862f2354c4b76afe755518b1fab986c8ce7a
Seb <s.dieunidou@gmail.com>
2020-10-17 11:53:33 +0200

    add suggest command

---
+++ commands/mtxserv/suggest.js
@@ -0,0 +1,59 @@
+const mTxServCommand = require('../mTxServCommand.js');
+const Discord = require('discord.js');
+
+module.exports = class SuggestCommand extends mTxServCommand {
+    constructor(client) {
+        super(client, {
+            name: 'suggest',
+            aliases: ['feedback'],
+            group: 'mtxserv',
+            memberName: 'suggest',
+            description: 'Submit a feedback',
+            clientPermissions: ['SEND_MESSAGES'],
+            guarded: true,
+            args: [{
+                key: 'suggestion',
+                prompt: 'What would you like to suggest?',
+                type: 'string',
+                validate: text => text.length >= 5,
+            }],
+            throttling: {
+                usages: 2,
+                duration: 5,
+            },
+        });
+    }
+
+    async run(msg, {suggestion}) {
+        const userLang = this.resolveLangOfMessage(msg)
+        const lang = require(`../../languages/${userLang}.json`)
+
+        const channelId = userLang == 'fr' ? '537921288463712259' : '537921288463712259'
+
+        const embed = new Discord.MessageEmbed()
+            .setAuthor(`${msg.author.tag}`, `${msg.author.displayAvatarURL()}`)
+            .setColor('DARK_BLUE')
+            .setDescription(suggestion.trim())
+            .setTimestamp();
+
+        if (msg.channel.type !== 'dm') {
+            embed
+                .addField('Guild', `${msg.guild.name}`, true)
+                .addField('Channel', `${msg.channel.name}`, true)
+            ;
+        }
+
+        msg.delete()
+
+        const resMsg = await this.client.channels.cache
+            .get(channelId)
+            .send({ embed })
+        ;
+
+        resMsg.react('👍');
+        resMsg.react('👎');
+        resMsg.react('🤷');
+
+        return this.saySuccess(msg, lang['suggest']['confirm'].replace('%name%', `<@${msg.author.id}>`))
+    }
+};
