9dae6524ac909a8a342f9c045e042a2d5a73327b
Seb <s.dieunidou@gmail.com>
2020-10-17 17:42:11 +0200

    fix

--- commands/admin/update.js
+++ commands/admin/update.js
@@ -28,7 +28,7 @@ module.exports = class ExecCommand extends mTxServCommand {
                 const embed = new Discord.MessageEmbed()
                     .setAuthor(`${this.client.user.tag}`, `${this.client.user.displayAvatarURL()}`)
                     .setColor(results.err ? 'RED' : 'GREEN')
-                    .setTitle(`:up: Updating bot..\`\`\`sh\n${results.std}\n\`\`\``)
+                    .setTitle(`:up: Updating bot..\n\`\`\`sh\n${results.std}\n\`\`\``)
                     .setTimestamp();
 
                 client
