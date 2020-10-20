0c74577c8a0f93849f0a1cb4f38869960c5e1d71
Seb <s.dieunidou@gmail.com>
2020-10-20 10:15:06 +0200

    update help

--- commands/mtxserv/help.js
+++ commands/mtxserv/help.js
@@ -97,11 +97,12 @@ module.exports = class HelpCommand extends mTxServCommand {
 					__**${showAll ? 'All commands' : `Available commands in ${msg.guild || 'this DM'}`}**__
 
 					${groups.filter(grp => grp.commands.some(cmd => !cmd.hidden && cmd.isUsable(msg)))
-						.map(grp => stripIndents`**❯  __${grp.name}__**
+						.map(grp => stripIndents`
+							**❯  __${grp.name}__**
 							${grp.commands.filter(cmd => !cmd.hidden && cmd.isUsable(msg))
 							.map(cmd => `\`${cmd.name}\``)
 						}
-						`).join('\n\n')
+						`).join('\n')
 					}
 				`.substr(0, 2048))
 				;
