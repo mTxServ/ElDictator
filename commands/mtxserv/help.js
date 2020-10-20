483cda203a57f7a1642e638c9341caf5df667cc5
Seb <s.dieunidou@gmail.com>
2020-10-20 10:37:25 +0200

    update help

--- commands/mtxserv/help.js
+++ commands/mtxserv/help.js
@@ -85,29 +85,18 @@ module.exports = class HelpCommand extends mTxServCommand {
 					.setTitle(':question: Help')
 					.setColor('BLUE')
 					.setDescription(stripIndents`
-					${oneLine`
-						To run a command, use ${mTxServCommand.usage('command', msg.guild ? msg.guild.commandPrefix : null, this.client.user)}.
-						, for example, ${mTxServCommand.usage('help', msg.guild ? msg.guild.commandPrefix : null, this.client.user)}.
-					`}
+					${lang['fork_me']['configure_explain']}
+
+					__**All commands**__
+					Use \`m!help <command>\` to view detailed information about a specific command.
 					
-					To run a command in this DM, simply use ${mTxServCommand.usage('command', null, null)} with no prefix.
-
-					Use ${this.usage('<command>', null, null)} to view detailed information about a specific command.
-
-					__**${showAll ? 'All commands' : `Available commands in ${msg.guild || 'this DM'}`}**__
-
-					${groups.filter(grp => grp.commands.some(cmd => !cmd.hidden && cmd.isUsable(msg)))
-						.map(grp => stripIndents`
-							**❯  __${grp.name}__**
-							${grp.commands.filter(cmd => !cmd.hidden && cmd.isUsable(msg))
-							.map(cmd => `\`${cmd.name}\``)
-						}
-						`).join('\n')
-					}
 				`.substr(0, 2048))
 				;
 
-				if(msg.channel.type !== 'dm') await msg.reply(lang['help']['sent_dm']);
+				const filteredGroups = groups.filter(grp => grp.commands.some(cmd => !cmd.hidden && cmd.isUsable(msg))).values()
+				for (const group of filteredGroups) {
+					embed.addField(`❯ ${group.name}`, group.commands.map(cmd => `\`${cmd.name}\``).join(' '));
+				}
 
 				return msg.say({
 					embed
