44feec5dae4ece7c0360eba4fab94116ff8848c0
Seb <sdieunidou@users.noreply.github.com>
2020-11-02 19:38:04 +0100

    Update giveaway.js

--- commands/mtxserv/giveaway.js
+++ commands/mtxserv/giveaway.js
@@ -49,7 +49,7 @@ module.exports = class GiveawayCommand extends mTxServCommand {
         let giveawayMsg = null
 
         if (this.client.isMainGuild(msg.guild.id) && this.client.isOwner(msg.author)) {
-            embed.setDescription(`Tirage au sort le **${endDate}**\n\n:four_leaf_clover: **Participer et Augmenter ses chances** :four_leaf_clover:\n\n${actions.join('\n')}\n\n:gift_heart: **Lots** :gift_heart:\n\n${prizeLabel}`)
+            embed.setDescription(`:four_leaf_clover: Pour participer, réagissez avec :gift: à ce message.\n\nTirage au sort le **${endDate}**\n\n:four_leaf_clover: **Participer et Augmenter ses chances** :four_leaf_clover:\n\n${actions.join('\n')}\n\n:gift_heart: **Lots** :gift_heart:\n\n${prizeLabel}`)
 
             const channel = await this.client.channels.cache.get('563304015924953108')
             giveawayMsg = false // await channel.messages.fetch('770702367049252874').catch(console.error)
