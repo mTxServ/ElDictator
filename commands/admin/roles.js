cb499094a3b2a076c856306c386df2721f7144f7
Seb <s.dieunidou@gmail.com>
2020-11-04 15:00:50 +0100

    show roles  stats

---
+++ commands/admin/roles.js
@@ -0,0 +1,32 @@
+const mTxServCommand = require('../mTxServCommand.js');
+const Discord = require('discord.js')
+
+module.exports = class BotStopCommand extends mTxServCommand {
+    constructor(client) {
+        super(client, {
+            name: 'roles',
+            group: 'admin',
+            memberName: 'roles',
+            description: 'Show roles stats.',
+            clientPermissions: ['SEND_MESSAGES'],
+            ownerOnly: true,
+            hidden: true,
+            guarded: true
+        });
+    }
+
+    async run(msg) {
+        const embed = new Discord.MessageEmbed()
+            .setAuthor(`${this.client.user.tag}`, `${this.client.user.displayAvatarURL()}`)
+            .setColor('RED')
+            .setTimestamp();
+
+        msg.guild.roles.map(role => {
+            embed.addField(role.name, role.members.size)
+        })
+
+        return msg.say({
+            embed
+        })
+    }
+};
