4af2b1a61cac650feedef05f0ffe7a8734a0f4d0
Seb <s.dieunidou@gmail.com>
2020-12-02 20:04:57 +0100

    update

--- commands/mtxserv/giveaway.js
+++ commands/mtxserv/giveaway.js
@@ -32,7 +32,7 @@ module.exports = class GiveawayCommand extends mTxServCommand {
             '> **+30 points**・Boostez le serveur discord de mTxServ',
             '> **+30 points**・Suivez le channel <#563304015924953108> sur votre serveur discord',
             '> **+10 points**・Réagissez à ce message avec :gift:',
-            '> **+10 points**・Retweetez le [message sur twitter](https://twitter.com/mTxServ/status/1327650869656117251) et suivez le compte [@mTxServ](https://twitter.com/mTxServ)',
+            '> **+10 points**・Retweetez le [message sur twitter](https://twitter.com/mTxServ/status/1334211860598558721) et suivez le compte [@mTxServ](https://twitter.com/mTxServ)',
             '> **+10 points**・[Invitez le <#769619263078006844> sur votre discord](https://discord.com/oauth2/authorize?client_id=535435520394657794&permissions=912577&scope=bot) puis poster le message du giveaway  avec \`m!giveaway\` sur son serveur',
         ]
 
